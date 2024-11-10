0ebe7674c821760614bceb219ad6855551cda176
BaldevBal <baldev.balraj@bellevuecollege.edu>
2024-11-09 17:49:28 -0800

    Update script.js

--- m07/script.js
+++ m07/script.js
@@ -12,7 +12,7 @@ function Hotel(name, rooms, booked) {
 // Create two hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-var sunsetRooms = new Rooms('Sunset', 86, 10);
+/*var sunsetRooms = new Rooms('Sunset', 86, 10);*/
 
 
 // Update the HTML for the page
@@ -26,10 +26,10 @@ var details2 = parkHotel.name + ' rooms: ';
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
-var details3 = sunsetRooms.name + ' rooms: ';
+/*var details3 = sunsetRooms.name + ' rooms: ';
     details3 += sunsetRooms.checkAvailability();
 var elHotel3 = document.getElementById('hotel3');
-elHotel3.textContent = details3;
+elHotel3.textContent = details3;*/
 
 /* 
 NOTE: textContent does not work in IE8 or earlier
