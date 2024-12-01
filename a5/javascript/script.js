d448e2cb10482e2d5cfdffb4e63feda154c3f85a
BaldevBal <baldev.balraj@bellevuecollege.edu>
2024-11-30 21:39:13 -0800

    Update script.js

--- a5/javascript/script.js
+++ a5/javascript/script.js
@@ -1,5 +1,10 @@
 const images = ["https://baldevbal.github.io/dev109/a5/images/artione.png", "https://baldevbal.github.io/dev109/a5/images/artitwo.png", "https://baldevbal.github.io/dev109/a5/images/artithree.png", "https://baldevbal.github.io/dev109/a5/images/artifour.png", "https://baldevbal.github.io/dev109/a5/images/artifive.png"];
-const captions = ["Artifact 1 - Resume", "Artifact 2 - Chessboard", "Artifact 3 - Rhombus", "Artifact 4 - Form Validation", "Artifact 5 - Slideshow"];
+
+const captions = ["Artifact 1 - Resume<br>The introduction artifact. The code only consists of HTML and basic text formatting.", 
+                  "Artifact 2 - Chessboard<br> First artifact with CSS and styling", 
+                  "Artifact 3 - Rhombus<br>", 
+                  "Artifact 4 - Form Validation<br>", 
+                  "Artifact 5 - Slideshow<br>"];
 let currentIndex = 0;
 
 const slideshowImage = document.getElementById('slideshowImage');
@@ -9,6 +14,7 @@ const caption = document.getElementById('caption');
 
 function updateImage() {
     slideshowImage.src = images[currentIndex];
+    caption.innerHTML = captions[currentIndex];
 }
 
 function nextImage() {
