3f0eb51efa76fc9a784d480822836f375b1bde1c
BaldevBal <baldev.balraj@bellevuecollege.edu>
2024-11-30 20:52:37 -0800

    Update script.js

--- a5/javascript/script.js
+++ a5/javascript/script.js
@@ -1,4 +1,4 @@
-const images = ["https://baldevbal.github.io/dev109/a1/images/artione.png", "https://baldevbal.github.io/dev109/a1/images/artitwo.png", "https://baldevbal.github.io/dev109/a1/images/artithree.png", "https://baldevbal.github.io/dev109/a1/images/artifour.png", "https://baldevbal.github.io/dev109/a1/images/artifive.png"]; // Replace with your image file names
+const images = ["https://baldevbal.github.io/dev109/a5/images/artione.png", "https://baldevbal.github.io/dev109/a5/images/artitwo.png", "https://baldevbal.github.io/dev109/a5/images/artithree.png", "https://baldevbal.github.io/dev109/a5/images/artifour.png", "https://baldevbal.github.io/dev109/a5/images/artifive.png"];
 const captions = ["Artifact 1 - Resume", "Artifact 2 - Chessboard", "Artifact 3 - Rhombus", "Artifact 4 - Form Validation", "Artifact 5 - Slideshow"];
 let currentIndex = 0;
 
@@ -22,12 +22,12 @@ function prevImage() {
 }
 
 nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % images.length; // Loop back to first image
+    currentIndex = (currentIndex + 1) % images.length;
     updateImage();
 });
 
 prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length; // Loop to last image
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImage();
 });
 let autoSlideInterval = setInterval(nextImage, 5500);
