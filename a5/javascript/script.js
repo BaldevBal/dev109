88eaab4844bffa8ce07e0b3b6dd91db8ae9e1c08
BaldevBal <baldev.balraj@bellevuecollege.edu>
2024-11-30 20:37:55 -0800

    Update script.js

--- a5/javascript/script.js
+++ a5/javascript/script.js
@@ -1,12 +1,24 @@
 const images = ["https://baldevbal.github.io/dev109/midterm/images/resumeartifact.png", "https://baldevbal.github.io/dev109/finalp1/images/shoppingcartimg.png", "https://baldevbal.github.io/dev109/finalp1/images/tableimg.png"]; // Replace with your image file names
+const captions = ["Hello, Artifact 1", "Hello, Artifact 2", "Hello, Artifact 3"];
 let currentIndex = 0;
 
-const slideshowImage = document.getElementById('slideshow-image');
+const slideshowImage = document.getElementById('slideshowImage');
 const nextButton = document.getElementById('next');
 const prevButton = document.getElementById('prev');
+const caption = document.getElementById('caption');
 
 function updateImage() {
     slideshowImage.src = images[currentIndex];
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
 }
 
 nextButton.addEventListener('click', () => {
@@ -18,5 +30,5 @@ prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length; // Loop to last image
     updateImage();
 });
-autoSlideInterval = setInterval(nextImage, 4500);
+let autoSlideInterval = setInterval(nextImage, 5500);
 updateImage();
